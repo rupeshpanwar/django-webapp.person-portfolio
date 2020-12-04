d47aad20d49c37a8abfa901975bc94f95326a818
trackscs <trackscs@outlook.com>
2020-12-04 21:57:55 +0530

    pushing to prod

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    blogtitle = models.CharField(max_length = 100)
+    date = models.DateField(auto_now=True)
+    blogdescription = models.CharField(max_length = 1000)
+
+
+    def __str__(self):
+        return self.blogtitle
--- portfolio/models.py
+++ portfolio/models.py
@@ -4,4 +4,8 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
-    image = models.URLField(blank=True)
+    url = models.URLField(blank=True)
+
+
+    def __str__(self):
+        return self.title
